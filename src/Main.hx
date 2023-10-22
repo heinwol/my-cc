01aae98cf618b54d6afc841c218bcd3a5d1f2714
heinwol <heinvvol@gmail.com>
2023-10-23 02:15:04 +0300

    cool, it builds stuff!

--- src/Main.hx
+++ src/Main.hx
@@ -1,6 +1,24 @@
-// import ;
+import cc.Peripheral;
+
+class SomeClass {
+	public var state:Int;
+
+	public function new(state:Int) {
+		this.state = state;
+	}
+
+	public function sth(x:Int):Int {
+		return x + 1 + state;
+	}
+}
+
 class Main {
 	static function main() {
 		trace("Haxe is great!");
+		var x = Peripheral.find("top", function(s:String, t:Int) {
+			return s.length == 1;
+		});
+		// x.
+		var y = new SomeClass(3);
 	}
 }
