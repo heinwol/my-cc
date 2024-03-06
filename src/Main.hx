48eb86b5b8d965a611e02cf1af077422debf88da
heinwol <heinvvol@gmail.com>
2024-03-06 15:34:15 +0300

    some initial stuff

--- src/Main.hx
+++ src/Main.hx
@@ -1,4 +1,49 @@
 import cc.Peripheral;
+import cc.Turtle;
+import Union.TrustedUnion;
+
+using lua.Table;
+
+enum Direction {
+	Top;
+	Bottom;
+}
+
+enum DirectionAbsolute {
+	Top;
+	Bottom;
+	Left;
+	Right;
+	Front;
+	Back;
+}
+
+typedef AnyDirection = TrustedUnion<Direction, DirectionAbsolute>;
+
+private function getDirection(d:AnyDirection):String {
+	return switch d.type() {
+		case Direction(d_):
+			switch d_ {
+				case Top: "Top";
+				case Bottom: "Bottom";
+			};
+		case DirectionAbsolute(d_):
+			switch d_ {
+				case Top: "Top";
+				case Bottom: "Bottom";
+				case Left: "Left";
+				case Right: "Right";
+				case Front: "Front";
+				case Back: "Back";
+			};
+	};
+}
+
+class MyPeripheral extends Peripheral {
+	public function wrapDirection<T>(d:Direction):T {
+		return Peripheral.wrap(getDirection(d));
+	}
+}
 
 class SomeClass {
 	public var state:Int;
@@ -15,9 +60,11 @@ class SomeClass {
 class Main {
 	static function main() {
 		trace("Haxe is great!");
-		var x = Peripheral.find("top", function(s:String, t:Int) {
+		var x = Peripheral.find("top", (s, t:Int) -> {
 			return s.length == 1;
 		});
+		// x.sort()
+		// Table.
 		// x.
 		var y = new SomeClass(3);
 	}
